73922e2f1a8365813092be471e3544c2cbec605a
gangsta-king <coolpirate2023@gmail.com>
2026-09-22 09:44:01 +0600

    first commit

---
+++ notes-21-09-26/app.py
@@ -0,0 +1,77 @@
+# print('Hello \nWorld!')
+
+# a = 45 #integer int 
+# b = 4.5 #decimal float
+# c='hello' #text string str
+# print(type(a),type(b), type(c))
+
+# import math
+# print(math.ceil(4.3)) #rounds up
+# print(math.floor(4.7)) #rounds down
+# print(math.sqrt(36)) #square root
+# print(math.pow(2,3)) #power
+# print(math.pi) #pi
+# print(math.e) #e
+# print(math.sin(math.pi/2)) #sine
+# print(math.cos(math.pi)) #cosine
+# print(math.tan(math.pi/4)) #tangent
+# print(math.log(100,10)) #logarithm base 10
+# print(math.log(100)) #natural logarithm
+# print(math.factorial(5)) #factorial
+# print(math.gcd(12, 15)) #greatest common divisor
+# print(math.lcm(12, 15)) #least common multiple
+# print(math.isqrt(16)) #integer square root
+# print(math.degrees(math.pi/2)) #convert radians to degrees
+# print(math.radians(90)) #convert degrees to radians
+# print(math.hypot(3, 4)) #hypotenuse
+# print(math.dist((1, 2), (4, 6))) #distance between two points
+# print(math.comb(5, 2)) #combinations
+# print(math.perm(5, 2)) #permutations
+# print(math.prod([1, 2, 3, 4])) #product of a list
+# print(math.fsum([0.1, 0.2, 0.3])) #accurate floating point sum
+# print(math.isclose(0.1 + 0.2, 0.3)) #check if two numbers are close
+# print(math.copysign(1, -5)) #copy sign
+# print(math.frexp(8)) #mantissa and exponent
+# print(math.ldexp(0.5, 3)) #multiply by power of two
+# print(math.modf(3.14)) #fractional and integer parts
+# print(math.nextafter(1.0, 2.0)) #next floating point number
+# print(math.ulp(1.0)) #unit in the last place
+# print(math.remainder(5, 3)) #remainder
+# print(math.trunc(3.9)) #truncate
+# print(math.fmod(5, 3)) #floating point modulus
+# print(math.exp(1)) #e raised to the power of 1
+# print(math.expm1(1)) #e raised to the power of 1 minus 1
+# print(math.log1p(1)) #natural logarithm of 1 plus x
+# print(math.log2(8)) #logarithm base 2
+# print(math.log10(100)) #logarithm base 10
+# print(math.isfinite(1.0)) #check if number is finite
+# print(math.isinf(float('inf'))) #check if number is infinite
+# print(math.isnan(float('nan'))) #check if number is NaN
+# print(math.copysign(1, -5)) #copy sign
+# print(math.fabs(-5)) #absolute value
+# print(math.factorial(5)) #factorial
+# print(math.gamma(5)) #gamma function
+# print(math.lgamma(5)) #log gamma function
+# print(math.erf(1)) #error function
+# print(math.erfc(1)) #complementary error function
+# print(math.isclose(0.1 + 0.2, 0.3)) #check if two numbers are close
+# print(math.isqrt(16)) #integer square root
+# print(math.prod([1, 2, 3, 4])) #product of a list
+
+
+# import random
+# print(random.random()) # 0 - 1
+# print(random.randint(100)) # 0 - 100 integer
+
+# minutes=int(input())
+# print(f'{minutes//60} hours, {minutes%60} minutes')
+
+# n=4567
+# nfirst=n%10
+# nsecond=(n//10)%10
+# nthird=(n//100)%10
+# nlast=(n//1000)%10
+
+n,m,k = int(input()),int(input()),int(input())
+if k//m > n:
+    print('Yes')
