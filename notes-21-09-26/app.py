5e5301e3192a10fa236ea4ac85666a88cd87c161
gangsta-king <coolpirate2023@gmail.com>
2026-09-25 10:37:30 +0600

    new commit

--- notes-21-09-26/app.py
+++ notes-21-09-26/app.py
@@ -144,8 +144,11 @@
 # n=input().split()
 # print(max(list(len(i) for i in n)))
 
-a,b,n=int(input()),int(input()),int(input())
-if (n%b==0 and n//b < a) or (n%a==0 and n//a < b):
-    print('YES')
-else:
-    print('NO')
+# a,b,n=int(input()),int(input()),int(input())
+# if (n%b==0 and n//b < a) or (n%a==0 and n//a < b):
+#     print('YES')
+# else:
+#     print('NO')
+
+# v,n=int(input()),int(input())
+# print(v*n % 109)
